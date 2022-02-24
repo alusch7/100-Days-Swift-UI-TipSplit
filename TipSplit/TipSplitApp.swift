e7d0879804a95ba558ec5f6b99e797ebe6cbd8cd
alusch7 <luschera@mcmaster.ca>
2022-02-23 21:13:27 -0500

    Initial Commit

---
+++ TipSplit/TipSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipSplitApp.swift
+//  TipSplit
+//
+//  Created by Alden Luscher on 2022-02-23.
+//
+
+import SwiftUI
+
+@main
+struct TipSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
